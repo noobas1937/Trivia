f2a7a712cc6a1d5ad4931d0dfb21c53a9caea6a8
Chenyu <920901835@qq.com>
2017-12-05 13:26:45 +0800

    syn

--- trivia-web/src/main/java/com/ecnu/trivia/web/common/websocket/WebSocketHandler.java
+++ trivia-web/src/main/java/com/ecnu/trivia/web/common/websocket/WebSocketHandler.java
@@ -3,11 +3,13 @@ package com.ecnu.trivia.web.common.websocket;
 import com.ecnu.trivia.web.rbac.service.SessionService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.session.Session;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -19,38 +21,40 @@ import java.util.List;
 public class WebSocketHandler extends TextWebSocketHandler {
 
     private static Logger logger = LoggerFactory.getLogger(SessionService.class);
-    /**存储在线用户 */
-    private final static List<WebSocketSession> SESSIONS = Collections.synchronizedList(new ArrayList<WebSocketSession>());
 
-    //消息处理
+    /**存储在线用户*/
+    private final static List<WebSocketSession> ONLINE_USER = Collections.synchronizedList(new ArrayList<WebSocketSession>());
+
+    /**消息处理*/
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+
         super.handleTextMessage(session, message);
     }
 
-    //连接建立后处理
+    /**连接建立后处理*/
     @SuppressWarnings("unchecked")
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         logger.debug("session:"+session.getId()+" | connect to the websocket success......");
-        SESSIONS.add(session);
+        ONLINE_USER.add(session);
     }
 
-    //抛出异常时处理
+    /**抛出异常时处理*/
     @Override
     public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
         if(session.isOpen()){
             session.close();
         }
         logger.debug("session:"+session.getId()+" | connection closed......");
-        SESSIONS.remove(session);
+        ONLINE_USER.remove(session);
     }
 
-    //连接关闭后处理
+    /**连接关闭后处理*/
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus closeStatus) throws Exception {
         logger.debug("session:"+session.getId()+" | connection closed......");
-        SESSIONS.remove(session);
+        ONLINE_USER.remove(session);
     }
 
     @Override
