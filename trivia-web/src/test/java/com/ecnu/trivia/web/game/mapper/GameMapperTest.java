9becdab77b54daef4855d966285fd584e14c8728
guoshuyi <guoshy.sx@fosun.com>
2018-01-04 22:32:46 +0800

    1

--- trivia-web/src/test/java/com/ecnu/trivia/web/game/mapper/GameMapperTest.java
+++ trivia-web/src/test/java/com/ecnu/trivia/web/game/mapper/GameMapperTest.java
@@ -1,5 +1,6 @@
 package com.ecnu.trivia.web.game.mapper;
-
+/*author:miss guo
+  date:2018.1.4*/
 import com.ecnu.trivia.web.game.domain.Game;
 import com.ecnu.trivia.web.room.mapper.RoomMapper;
 import org.junit.Test;
@@ -24,8 +25,8 @@ public class GameMapperTest {
     public void getGameByRoomId() throws Exception {
         Integer roomid=1;
         Integer roomid1=5;
-       Game res=gameMapper.getGameByRoomId(roomid);
-        Game res1=gameMapper.getGameByRoomId(roomid);
+        Game res=gameMapper.getGameByRoomId(roomid);
+        Game res1=gameMapper.getGameByRoomId(roomid1);
        if(res!=null){
            AssertJUnit.fail("fail ");
        }
@@ -36,21 +37,48 @@ public class GameMapperTest {
 
     @Test
     public void updateGameStatus() throws Exception {
-        gameMapper.updateGameStatus();
-
-
+        gameMapper.updateGameStatus(23,79,1,3,1);
+        Integer CurrentPlayerId=gameMapper.getGameByRoomId(5).getCurrentPlayerId();
+        Integer dicenumber=gameMapper.getGameByRoomId(5).getDiceNumber();
+        Integer quesid=gameMapper.getGameByRoomId(5).getQuestionId();
+        Integer stage=gameMapper.getGameByRoomId(5).getStage();
+        if(CurrentPlayerId!=79){
+            AssertJUnit.fail("fail");
+        }
+        if(dicenumber!=1){
+            AssertJUnit.fail("fail");
+        }
+        if(quesid!=3){
+            AssertJUnit.fail("fail");
+        }
+        if(stage!=1){
+            AssertJUnit.fail("fail");
+        }
     }
 
     @Test
     public void getGameByQuestionId() throws Exception {
+        Game res=gameMapper.getGameByQuestionId(17);
+        if(res==null){
+            AssertJUnit.fail("fail");
+        }
     }
 
     @Test
     public void addGame() throws Exception {
+        gameMapper.addGame(10,79);
+        Game res=gameMapper.getGameByRoomId(10);
+        if(res==null){
+            AssertJUnit.fail("fail");
+        }
     }
 
     @Test
     public void getAppropriateReadyRoomId() throws Exception {
+        Integer res=gameMapper.getAppropriateReadyRoomId(4);
+        if(res!=3){
+            AssertJUnit.fail("fail");
+        }
     }
 
 }
